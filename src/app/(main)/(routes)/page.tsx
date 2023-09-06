c17ca61592f9e5fd5d08c6b4e4d6486d3f08871e
danillyamkin <danilwebdev@gmail.com>
2023-09-06 14:35:41 +0300

    feat(themes): add toggle theme button

--- src/app/(main)/(routes)/page.tsx
+++ src/app/(main)/(routes)/page.tsx
@@ -1,9 +1,12 @@
 import { UserButton } from '@clerk/nextjs'
 
+import { ToggleTheme } from '@/features/toggle-theme'
+
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ToggleTheme />
     </div>
   )
 }
