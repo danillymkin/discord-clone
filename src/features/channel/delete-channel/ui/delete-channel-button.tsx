fae7b930c67190a5a974a34b4c6ce8d2a3c09d81
danillyamkin <danilwebdev@gmail.com>
2023-09-16 18:35:37 +0300

    fix: add prevent default

--- src/features/channel/delete-channel/ui/delete-channel-button.tsx
+++ src/features/channel/delete-channel/ui/delete-channel-button.tsx
@@ -4,7 +4,10 @@ import { Chanel, Server } from '@prisma/client'
 import { Trash } from 'lucide-react'
 import { useContext } from 'react'
 
-import { GlobalModalsContext } from '@/shared/lib/context/global-modals-context'
+import {
+  GlobalModalsContext,
+  ModalType,
+} from '@/shared/lib/context/global-modals-context'
 import {
   Tooltip,
   TooltipContent,
@@ -23,15 +26,18 @@ export const DeleteChannelButton = ({
 }: DeleteChannelButtonProps) => {
   const { onOpen } = useContext(GlobalModalsContext)
 
+  const onAction = (e: React.MouseEvent, action: ModalType) => {
+    e.stopPropagation()
+    e.preventDefault()
+    onOpen(action, { server, channel })
+  }
+
   return (
     <TooltipProvider>
       <Tooltip delayDuration={50}>
         <TooltipTrigger asChild>
           <button
-            onClick={(event) => {
-              event.preventDefault()
-              onOpen('deleteChannel', { channel, server })
-            }}
+            onClick={(e) => onAction(e, 'deleteChannel')}
             className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
           >
             <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
