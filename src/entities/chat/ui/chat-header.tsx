b049a73b3e69e4bf69fad15cea82a2af6f439139
danillyamkin <danilwebdev@gmail.com>
2023-09-18 17:01:29 +0300

    feat: create chat-header component

---
+++ src/entities/chat/ui/chat-header.tsx
@@ -0,0 +1,29 @@
+import { Hash } from 'lucide-react'
+
+interface ChatHeaderProps {
+  mobileMenu: React.ReactNode
+  name: string
+  type: 'channel' | 'conversation'
+  avatar?: React.ReactNode
+}
+
+export const ChatHeader = ({
+  name,
+  type,
+  avatar,
+  mobileMenu,
+}: ChatHeaderProps) => {
+  return (
+    <div className="font-semibold text-md px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
+      {mobileMenu}
+
+      {type === 'channel' && (
+        <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+      )}
+
+      {type === 'conversation' && avatar}
+
+      <p className="font-semibold text-md text-black dark:text-white">{name}</p>
+    </div>
+  )
+}
