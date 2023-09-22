688746dcf6baf202a226c4d0f600ea601118bce5
danillyamkin <danilwebdev@gmail.com>
2023-09-22 15:38:06 +0300

    feat: add different variant for message file

--- src/shared/ui/upload-file/ui.tsx
+++ src/shared/ui/upload-file/ui.tsx
@@ -4,6 +4,7 @@ import '@uploadthing/react/styles.css'
 import { X } from 'lucide-react'
 import Image from 'next/image'
 
+import { cn } from '@/shared/lib/cn'
 import { UploadDropzone } from '@/shared/lib/uploadthing'
 
 interface UploadFileProps {
@@ -17,12 +18,25 @@ export const UploadFile = ({ endpoint, value, onChange }: UploadFileProps) => {
 
   if (value && fileType !== 'pdf') {
     return (
-      <div className="relative h-20 w-20 border border-zinc-300 rounded-full">
-        <Image fill src={value} alt="Upload" className="rounded-full" />
+      <div
+        className={cn(
+          'relative h-20 w-20 border border-zinc-300',
+          endpoint === 'serverImage' && 'rounded-full'
+        )}
+      >
+        <Image
+          fill
+          src={value}
+          alt="Upload"
+          className={cn(endpoint === 'serverImage' && 'rounded-full')}
+        />
         <button
           onClick={() => onChange('')}
           type="button"
-          className="bg-rose-500 text-white rounded-full p-1 absolute top-0 right-0 shadow-sm"
+          className={cn(
+            'bg-rose-500 text-white rounded-full p-1 absolute top-0 right-0 shadow-sm',
+            endpoint === 'messageFile' && '-top-2 -right-2'
+          )}
         >
           <X className="h-4 w-4" />
         </button>
